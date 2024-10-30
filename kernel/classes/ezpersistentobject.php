6884d1ffb5cec3e79e226a8a8c4bfb2de5e54cdd
Luca Realdi <luca.realdi@opencontent.it>
2024-10-30 13:32:19 +0100

    Avoid warning in persistent object length

--- kernel/classes/ezpersistentobject.php
+++ kernel/classes/ezpersistentobject.php
@@ -927,7 +927,7 @@ class eZPersistentObject
             {
                 $db_params["limit"] = $limit["limit"];
             }
-            else
+            elseif ( isset( $limit['length'] ) )
             {
                 $db_params["limit"] = $limit["length"];
             }
